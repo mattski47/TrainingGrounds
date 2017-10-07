440d79fa185bdc236fab1fe22ff87b9a7054d887
mattski47 <matthewsski@gmail.com>
2017-10-07 13:18:31 -0700

    Initial Commit

---
+++ Source/TrainingGrounds/TrainingGroundsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TrainingGroundsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATrainingGroundsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATrainingGroundsGameMode();
+};
+
+
+
